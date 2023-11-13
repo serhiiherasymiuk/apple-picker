d3f41c9278a9a79616e3cc0665cb72e8162d7be9
Serhii Herasymiuk <sgerasimuk07@gmail.com>
2023-11-13 23:12:07 +0200

    Complete game

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Set in Inspector")]
+    [SerializeField] GameObject applePrefab;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float leftAndRightEdge = 10f;
+    [SerializeField] float chanceToChangeDirections = 0.1f;
+    [SerializeField] float secondsBetweenAppleDrops = 1f;
+
     void Start()
     {
-        
+        StartCoroutine(DropApples());
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        MoveRandomly();
+    }
+
+    IEnumerator DropApples()
+    {
+        while (true)
+        {
+            Instantiate(applePrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(secondsBetweenAppleDrops);
+        }
+    }
+
+    void MoveRandomly()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+
+        if (pos.x < -leftAndRightEdge || pos.x > leftAndRightEdge)
+        {
+            ChangeDirection();
+        }
+        else
+        {
+            transform.position = pos;
+        }
+
+        if (Random.value < chanceToChangeDirections)
+        {
+            ChangeDirection();
+        }
+    }
+
+    void ChangeDirection()
+    {
+        speed *= -1;
     }
 }
--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Set Dynamically")]
+    TextMeshProUGUI scoreGT;
+    int score = 0;
+
+    private void Awake()
     {
-        
+        scoreGT = GameObject.Find("ScoreCounter").GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = transform.position;
+        pos.x = mousePos3D.x;
+        transform.position = pos;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GameObject collidedWith = other.gameObject;
+        if (collidedWith.layer == LayerMask.NameToLayer("Apples"))
+        {
+            Destroy(collidedWith);
+            score += 100;
+            scoreGT.text = "Score: " + score.ToString();
+            if (score > HighScore.score)
+            {
+                HighScore.score = score;
+            }
+        }
     }
 }
